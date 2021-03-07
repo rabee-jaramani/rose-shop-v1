4dfdd6353a1d9306ab95cfd23c36e21cd64bda3c
Rabee Aljaramani <rabeealjaramani@yahoo.com>
2021-03-07 15:10:43 +0400

    Update HEADER_MESSAGE.js

--- src/header_message/HEADER_MESSAGE.js
+++ src/header_message/HEADER_MESSAGE.js
@@ -7,7 +7,7 @@ export default function HEADER_MESSAGE() {
         <>
             <div className='hdr-msg-div'>
                 <h1>Roses For All Events, Onlie Rose Shop, Deliver In Dubai And All UAE
-                     <p>Call us on 009714545. Whatsapp: 0097161611</p></h1>
+                     <p>Call us on 9714545. Whatsapp: 97161611</p></h1>
                 <Link id='hdr_msg_btn' to='cards' href='cards' className='hdr-msg-btn'>Start The Journey</Link>
             </div>
         </>
