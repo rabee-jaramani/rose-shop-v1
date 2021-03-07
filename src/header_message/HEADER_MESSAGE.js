9ab2a6f500702efa5df25b4d8b8f6ad6e61b88e3
Rabee Aljaramani <rabeealjaramani@yahoo.com>
2021-03-07 15:50:10 +0400

    d

--- src/header_message/HEADER_MESSAGE.js
+++ src/header_message/HEADER_MESSAGE.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+// import {$} from 'react-jquery-plugin'
 
 export default function HEADER_MESSAGE() {
-
+function scroll() {
+    setTimeout(()=>{window.scrollTo({top: 650, behavior: 'smooth'})},100)
+}
     return (
         <>
             <div className='hdr-msg-div'>
                 <h1>Roses For All Events, Onlie Rose Shop, Deliver In Dubai And All UAE
                      <p>Call us on 9714545. Whatsapp: 97161611</p></h1>
-                <Link id='hdr_msg_btn' to='cards' href='cards' className='hdr-msg-btn'>Start The Journey</Link>
+                <Link onClick={scroll} id='hdr_msg_btn' to='cards' href='cards' className='hdr-msg-btn'>Start The Journey</Link>
             </div>
         </>
     )
