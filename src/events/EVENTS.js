a1a53e68bb66cc1cc361c3f9f01aa227795eadba
Rabee Aljaramani <rabeealjaramani@yahoo.com>
2021-03-08 16:32:52 +0400

    j

--- src/events/EVENTS.js
+++ src/events/EVENTS.js
@@ -15,37 +15,37 @@ export default function EVENTS() {
                 <div className='video'>
                     <ReactPlayer className='react-player'
                      width='100%'
-                        url="https://www.youtube.com/watch?v=0B-zDOdyGME"
+                        url="https://www.youtube.com/watch?v=1wbJEBuNWvY"
                     />
                 </div>
                 <div className='video'>
                     <ReactPlayer className='react-player'
                      width='100%'
-                        url="https://www.youtube.com/watch?v=0B-zDOdyGME"
+                        url="https://www.youtube.com/watch?v=EQNCmcvoQo4"
                     />
                 </div>
                 <div className='video'>
                     <ReactPlayer className='react-player'
                      width='100%'
-                        url="https://www.youtube.com/watch?v=0B-zDOdyGME"
+                        url="https://www.youtube.com/watch?v=73VnkzCLVU0"
                     />
                 </div>
                 <div className='video'>
                     <ReactPlayer className='react-player'
                      width='100%'
-                        url="https://www.youtube.com/watch?v=0B-zDOdyGME"
+                        url="https://www.youtube.com/watch?v=PWqJOE8HZUw"
                     />
                 </div>
                 <div className='video'>
                     <ReactPlayer className='react-player'
                      width='100%'
-                        url="https://www.youtube.com/watch?v=0B-zDOdyGME"
+                        url="https://www.youtube.com/watch?v=dFd4uAlfGZY"
                     />
                 </div>
                 <div className='video'>
                     <ReactPlayer className='react-player'
                      width='100%'
-                        url="https://www.youtube.com/watch?v=0B-zDOdyGME"
+                        url="https://www.youtube.com/watch?v=9uucte5e318"
                     />
                 </div>
                 <div className='video'>
