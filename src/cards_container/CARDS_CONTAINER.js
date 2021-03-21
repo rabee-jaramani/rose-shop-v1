0cf249d4976d8eb817c8b7509e3bcea4e0a46f7c
Rabee Aljaramani <rabeealjaramani@yahoo.com>
2021-03-21 15:57:40 +0400

    df

--- src/cards_container/CARDS_CONTAINER.js
+++ src/cards_container/CARDS_CONTAINER.js
@@ -11,7 +11,7 @@ state={
   items:[],
   item:{},
   inputValue:'',
-  items_:items
+  items_:items,
 }
 
 showCats=(e)=>{
@@ -26,6 +26,12 @@ showCats=(e)=>{
 valantine_handler=()=>{
   this.setState({
     items_:valantine_items
+  });
+  document.querySelector('.cat-1').classList.remove('focused');
+}
+forAll_handler=()=>{
+  this.setState({
+    items_:items
   })
 }
   onChangeHandler=(e)=>{
@@ -36,6 +42,7 @@ valantine_handler=()=>{
   }
 
   componentDidMount(){
+   
   }
     render(){
     return (
@@ -48,10 +55,9 @@ valantine_handler=()=>{
            
             
             <div className='cat-div'>
-            
-                  <div className='cat-1 '  contenteditable tabindex="1" onClick={this.valantine_handler}>Valantine's day</div>
-                  <div className='cat-2 '  contenteditable tabindex="2">Mother's day</div>
-                  <div className='cat-3 '  contenteditable tabindex="3">A birthday</div>
+                  <div className='cat-1 focused '  contenteditable tabindex="1" onClick={this.forAll_handler}>For All</div>
+                  <div className='cat-2 '  contenteditable tabindex="2" onClick={this.valantine_handler}>Valantine's day</div>
+                  <div className='cat-3 '  contenteditable tabindex="3">Mother's day</div>
                   <div className='cat-4 '  contenteditable tabindex="4">I'm sorry</div>
                 </div>
                 <div className='cat-tab' onClick={this.showCats}>Chose Your Occasions <i class="fas fa-arrow-circle-down "></i></div>
